e975b273917ad0589b184178731f819c4a514c9a
DiyanKalaydzhiev23 <diankostadenov@gmail.com>
2023-04-22 19:29:06 +0300

    Added home page

--- synapse/src/components/Home/Home.jsx
+++ synapse/src/components/Home/Home.jsx
@@ -1,7 +1,31 @@
+import { useState } from 'react';
+import HomeStyles from './Home.module.css';
+
 export default function Home() {
-    return (<>
+    const [displayVerticalBar, setDisplayVerticalBar] = useState('block');
 
-        
+    const removeVerticalLine = (e) => {
+        const inputValue = e.target.value;
+        const newDisplayValue = (inputValue ? 'none' : 'block')
+        setDisplayVerticalBar(newDisplayValue);
+    }
 
-    </>);
+    return (
+        <div>
+            <div id={HomeStyles.searchBarWrapper}>
+                <div id={HomeStyles.verticalBar} style={{display: displayVerticalBar}} ></div>
+                <div id={HomeStyles.searchBar}>
+                    <input onInput={removeVerticalLine} type="text" placeholder='Write something stupid' />
+                </div>
+            </div>
+
+            <div id={HomeStyles.outerTagsWrapper}>
+                <div id={HomeStyles.tagsWrapper}>
+                    <div className={HomeStyles.recentTag}>Tag one</div>
+                    <div className={HomeStyles.recentTag}>Tag two</div>
+                    <div className={HomeStyles.recentTag}>Tag three</div>
+                </div>
+            </div>
+        </div>
+    );
 }
